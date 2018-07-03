48622c73606e46faae4c0b615f043de5909efc43
myf <lxtx2013@foxmail.com>
2018-07-03 21:08:30 +0800

    完善保存文件方式

--- src/com/ccnu/nercel/file/Save.java
+++ src/com/ccnu/nercel/file/Save.java
@@ -27,19 +27,23 @@ public class Save {
 		// TODO Auto-generated constructor stub
 		Dimension imageSize = Paint.pnlDisplayArea.getSize();
 		jFileChooser=new JFileChooser();
+		//设置文件路径
 		jFileChooser.setCurrentDirectory(new File("./"));
-		jFileChooser.showSaveDialog(null);		
+		//设置默认文件名
+		jFileChooser.setSelectedFile(new File("未命名.png"));
+		//获取点击的是确定or取消
+		int returnVal = jFileChooser.showSaveDialog(null);		
 		BufferedImage image = new BufferedImage(imageSize.width,imageSize.height, BufferedImage.TYPE_INT_ARGB);
 		Graphics2D g = image.createGraphics();
 		Paint.pnlDisplayArea.paint(g);
         g.dispose();
         File fileW =jFileChooser.getSelectedFile();
-        
-        if(fileW != null) {
+//        System.out.println(jFileChooser.CANCEL_OPTION);
+        if(returnVal == jFileChooser.APPROVE_OPTION & fileW != null) {
         	try {
     			ImageIO.write(image,"png", fileW);
 //    			JOptionPane notice=new JOptionPane ();
-    			JOptionPane.showMessageDialog(null, "图片保存成功！","保存成功",JOptionPane.INFORMATION_MESSAGE);
+    			JOptionPane.showMessageDialog(null, "图片已保存！","保存成功",JOptionPane.INFORMATION_MESSAGE);
     			hassave=true;
     		} catch (FileNotFoundException e) {
     			// TODO Auto-generated catch block
